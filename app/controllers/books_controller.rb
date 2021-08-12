ec2710073bf3c6eed0ad08cdd940fa7051b8cbe9
yumekumo <chihachiha.ryotakun@gmail.com>
2021-08-12 17:26:39 +0000

    [Add] 投稿機能とindexの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
