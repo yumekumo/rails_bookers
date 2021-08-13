5cf70dad17230b667d79c49326e4cfd79d52613b
yumekumo <chihachiha.ryotakun@gmail.com>
2021-08-13 13:19:34 +0000

    [Add] バリテーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,25 +5,34 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:created] = 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:updated] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -34,4 +43,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
