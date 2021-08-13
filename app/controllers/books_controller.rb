2b48a98a71e58475a0bf04e0851671cdcefddef5
yumekumo <chihachiha.ryotakun@gmail.com>
2021-08-13 04:08:28 +0000

    [Add] 削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,12 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
   def book_params
